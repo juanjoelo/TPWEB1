5905d17e5516f99e60921ddab0517869670ef855
juanjoelo <juanjoel.olguin@gmail.com>
2023-10-09 18:01:57 -0300

    intento de carrusel

--- javascript.js
+++ javascript.js
@@ -4,3 +4,12 @@ function paginaInicio() {
 function paginaContacto() {
   window.location.href = "contacto.html";
 }
+const contenedor = document.querySelector(".contenedorCarrusel");
+const botonesIzquierda = document.querySelector(".izq");
+const botonesDerecha = document.querySelector(".der");
+
+let index = 0;
+
+function cambiarImagen(index) {
+  contenedor.style.transform = "translateX(-400%)";
+}
