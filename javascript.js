92b2a8f0e1c214a1385ea637be6fdaadb2110452
juanjoelo <juanjoel.olguin@gmail.com>
2023-10-09 15:10:42 -0300

    update contacto href javascript

--- javascript.js
+++ javascript.js
@@ -1,7 +1,6 @@
 function paginaInicio() {
   window.location.href = "https://juanjoelo.github.io/TPWEB1/";
 }
-function paginaContacto(){
-
-    window.location.href = "https//juanjoelo.github.io/TPWEB1/;"
+function paginaContacto() {
+  window.location.href = "contacto.html";
 }
